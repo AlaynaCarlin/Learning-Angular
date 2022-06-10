4f5263915d2823f12859f4b5d4269b1b0da26649
AlaynaCarlin <alaynacarlin18@gmail.com>
2022-06-10 13:47:41 -0400

    ngFor Directive

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,12 +10,7 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  value = 1.23;
-
-  hero: Hero = {
-    id: 1,
-    name: 'wind storm'
-  };
+  heroes = HEROES;
 
   constructor() { }
 
